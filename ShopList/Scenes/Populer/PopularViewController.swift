2d849fc558a63a9bf176db1edc7c20c88c2a65f0
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-07-03 12:37:23 +0300

    background color update

--- ShopList/Scenes/Populer/PopularViewController.swift
+++ ShopList/Scenes/Populer/PopularViewController.swift
@@ -30,7 +30,7 @@ class PopularViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .appBackground
         setupCollectionView()
         fetchCategories()
     }
