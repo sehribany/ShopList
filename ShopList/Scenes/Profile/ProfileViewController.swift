99b93c028cfa62e36f139bf492d10f5080ec15c3
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-07-03 16:31:27 +0300

    ProfileViewController Created.

---
+++ ShopList/Scenes/Profile/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  ShopList
+//
+//  Created by Şehriban Yıldırım on 2.07.2024.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+
+}
