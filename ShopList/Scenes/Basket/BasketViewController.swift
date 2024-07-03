2cf95947967f2ef2a116fd33f55ef04336c73885
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-07-03 16:30:50 +0300

    BasketViewController Created.

---
+++ ShopList/Scenes/Basket/BasketViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BasketViewController.swift
+//  ShopList
+//
+//  Created by Şehriban Yıldırım on 2.07.2024.
+//
+
+import UIKit
+
+class BasketViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
