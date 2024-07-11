bcdffe06c821af45efa755835e35be029ee4ac46
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-07-11 19:25:52 +0300

    Save a new list to Firestore

--- ShopList/Scenes/Lists/ListViewModel.swift
+++ ShopList/Scenes/Lists/ListViewModel.swift
@@ -6,7 +6,28 @@
 //
 
 import Foundation
+import Firebase
 
 class ListViewModel{
     
+    private let db = Firestore.firestore()
+    
+    var lists: [List] = []
+    
+    // Saves a new list to Firestore
+    func saveList(name: String, completion: @escaping (Error?) -> Void) {
+        let newList = List(id: UUID().uuidString, name: name, products: [])
+        db.collection("lists").document(newList.id).setData([
+            "id": newList.id,
+            "name": newList.name,
+            "products": []
+        ]) { error in
+            if let error = error {
+                completion(error)
+            } else {
+                self.lists.append(newList)
+                completion(nil)
+            }
+        }
+    }
 }
