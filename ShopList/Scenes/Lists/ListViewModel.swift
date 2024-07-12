acb5b464076ba20ae5f15783ea8f30f7b4ed2b4c
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-07-12 23:38:43 +0300

    fetches all lists from firestore

--- ShopList/Scenes/Lists/ListViewModel.swift
+++ ShopList/Scenes/Lists/ListViewModel.swift
@@ -12,7 +12,7 @@ class ListViewModel{
     
     private let db = Firestore.firestore()
     
-    var lists: [List] = []
+    var lists = [List]()
     
     // Saves a new list to Firestore
     func saveList(name: String, completion: @escaping (Error?) -> Void) {
@@ -26,6 +26,33 @@ class ListViewModel{
                 completion(error)
             } else {
                 self.lists.append(newList)
+                completion(nil)
+            }
+        }
+    }
+    
+    // Fetches all lists from Firestore
+    func fetchLists(completion: @escaping (Error?) -> Void) {
+        db.collection("lists").getDocuments { (snapshot, error) in
+            if let error = error {
+                completion(error)
+            } else {
+                self.lists = snapshot?.documents.compactMap { document in
+                    let data = document.data()
+                    guard let id = data["id"] as? String,
+                          let name = data["name"] as? String,
+                          let productsData = data["products"] as? [[String: Any]] else { return nil }
+                    
+                    let products = productsData.compactMap { productData in
+                        return Product(id: productData["id"] as? Int ?? 0,
+                                       name: productData["name"] as? String ?? "",
+                                       image: productData["image"] as? String ?? "",
+                                       categoryID: productData["categoryID"] as? Int ?? 0,
+                                       quantity: productData["quantity"] as? Int ?? 0)
+                    }
+                    
+                    return List(id: id, name: name, products: products)
+                } ?? []
                 completion(nil)
             }
         }
