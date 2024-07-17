4c3a5595b13a7631c3f25ee0cd24913102b52b7a
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-07-17 15:26:52 +0300

    ProductViewController Created.

---
+++ ShopList/Scenes/Product/ProductViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProductViewController.swift
+//  ShopList
+//
+//  Created by Şehriban Yıldırım on 13.07.2024.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+
+}
